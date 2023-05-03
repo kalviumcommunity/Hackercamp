85a60a96a356f0c407717e8a876c579723f22e08
jeeva parkkavan <jeeva.parkkavan@kalvium.community>
2023-05-03 13:38:57 +0530

    fix: fixed the typos in the tailwind config file and App.jsx to improve the consistency in the codebase

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -3,19 +3,17 @@ import Navbar from './components/navbar/navbar'
 import SearchBar from './components/searchbar/searchBar'
 import ClearFilter from './components/filters/clearFilter';
 import Filters from './components/filters/filters';
-import HackathonList from './components/hackathonLister/hackathonList';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import './App.css';
+
 function App() {
   
   return (
-      <div className="App bg-[#f8f9fa]">
-        
-          <Navbar />
-          <SearchBar />
-          <ClearFilter />
-          <Filters />
+      <div className="App bg-">
+              <Navbar />
+              <SearchBar />
+              <ClearFilter />
+              <Filters />
+
       </div>
   );
 }
