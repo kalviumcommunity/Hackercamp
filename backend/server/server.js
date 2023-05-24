a384bff38eed7dbbea0c00577574407161b8e32a
jeeva1429 <116168359+jeeva1429@users.noreply.github.com>
2023-05-24 14:17:53 +0530

    Update server.js
    Fix: Removed the unnecessary code form the file to improve the readability and consistency in the codebase

--- backend/server/server.js
+++ backend/server/server.js
@@ -6,8 +6,7 @@ const multer = require("multer");
 const upload = multer({storage: multer.memoryStorage() });
 const axios = require("axios");
 const FormData = require("form-data");
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsDoc = require("swagger-jsdoc");
+
 
 const mockedData = require("../../frontEnd/src/components/data.json");
 
@@ -25,66 +24,6 @@ app.get("/api/hackathons/:id", (req, res) => {
   res.json(filteredHackathons);
 });
 
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Hackathon API",
-      version: "1.0.0",
-      description: "API to manage hackathons",
-    },
-  },
-  apis:[__filename], // Specify the file path where your API routes are defined
-};
-const swaggerSpec = swaggerJsDoc(swaggerOptions);
-
-// Endpoint for Swagger UI
-app.use("/api-docs", swaggerUi.serve);
-app.get("/api-docs", swaggerUi.setup(swaggerSpec));
-// Endpoint for uploading an image
-/**
-   @swagger
- * /upload:
- *   post:
- *     summary: Upload an image and get the image URL
- *     requestBody:
- *       content:
- *         multipart/form-data:
- *           schema:
- *             type: object
- *             properties:
- *               image:
- *                 type: string
- *                 format: binary
- *     responses:
- *       200:
- *         description: Image uploaded successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 imageUrl:
- *                   type: string
- *       400:
- *         description: No image uploaded
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *       500:
- *         description: Something went wrong
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- */
 
 app.post("/api/upload", upload.single("image"), async (req, res) => {
   try {
